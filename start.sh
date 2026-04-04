679f8676d3c5f7780e6a4ff354ff82b90b70fcc3
poverzhukilya-ops <poverzhukilya@gmail.com>
2026-04-04 21:53:09 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# start.sh
-
-# Запускаем Flask API на порту 8080
-python -m flask --app bot:api_app run --host=0.0.0.0 --port=8080 &
+# Запускаем Flask API
+gunicorn --bind 0.0.0.0:8080 bot:api_app &
 
 # Запускаем бота
 python bot.py
