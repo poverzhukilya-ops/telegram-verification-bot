dce83038ee454d52844f12b996a4ff4c0c3961f3
poverzhukilya-ops <poverzhukilya@gmail.com>
2026-04-16 13:00:29 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "8592301276:AAG9kvpWi__OEoZ4mQ0muVdMoSMM
 GROUP_ID = int(os.environ.get("GROUP_ID", "-1003294227033"))
 INVITE_LINK = os.environ.get("INVITE_LINK", "https://t.me/+O8Nj8Ojy2QJkNzFi")
 ADMIN_ID = int(os.environ.get("ADMIN_ID", "8066467254"))
-REGULATIONS_LINK = os.environ.get("REGULATIONS_LINK", "http://avantyrist1188.netlify.app")
+REGULATIONS_LINK = os.environ.get("REGULATIONS_LINK", "https://avantyrist1188.onrender.com")
 CHANNEL_LINK = os.environ.get("CHANNEL_LINK", "https://t.me/+9H4yR1nBPRw5NzEy")
 GROUPS_FILE = "groups.json"
